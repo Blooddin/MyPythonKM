8ae8d1e4d9f9a11ade2acfb398cca2c2a9743b7b
Blooddin <romanovskoff@gmail.com>
2021-02-20 08:20:52 +0300

    Created a function for checking if a number is a zero, above or below zero

--- LR0/HelloWorldPython.py
+++ LR0/HelloWorldPython.py
@@ -1,13 +1,17 @@
 print('Hello World!')
 
+# Function for checking if a number is above, equal or below zero.  
+def check_if_zero(a):
+    if a>0:
+        print('More than zero.')
+    elif a == 0:
+        print("Indeed, a zero.")
+    else:
+        print('Less than zero.')
+
 # Number input  
 a = input("Input a number to check if it's more than a zero ")
 a = int(a)
 
-# Checking if a number is above, equal or below zero.  
-if a>0:
-    print('More than zero.')
-elif a == 0:
-    print("Indeed, a zero.")
-else:
-    print('Less than zero.')
+check_if_zero(a)
+
