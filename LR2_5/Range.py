1a0f9eb91f757d20a4501e4de9288d2d6498ca3f
Blooddin <romanovskoff@gmail.com>
2021-06-09 23:18:44 +0300

    Finished LR2_5

--- LR2_5/Range.py
+++ LR2_5/Range.py
@@ -3,46 +3,57 @@
 
 
 class myRange():
-	def __init__(self, *arr):
-		self.arr = arr
+    def __init__(self, start, end, *args):
+        self.arr = range(start, end, *args)
+    def __iter__(self):
+        return iter(self.arr)
+    def __reversed__(self):
+        return reversed(self.arr)
+    def __contains__(self, value):
+        for item in self.arr:
+            if item == value:
+                return True
+        return False
+    def __len__(self):
+        return(len(self.arr))
 
 
-def myRangeGenerator():
-	def range(start, end, *args):
-		arr = []
-		i = start
-		step = 1
 
-		if len(args):
-			if isinstance(args[0], int):
-				step = args[0]
+def range(start, end, *args):
+    arr = []
+    i = start
+    step = 1
 
-		while i < end:
-			arr.append(i)
-			i += step
+    if len(args):
+        if isinstance(args[0], int):
+            step = args[0]
 
-		if step<0:
-			while i > end:
-				arr.append(i)
-				i += step
+    while i < end:
+        arr.append(i)
+        i += step
 
-		if len(args):
-			if callable(args[0]):
-				arrres = []
-				for item in arr:
-					if args[0](item):
-						arrres.append(item)
-				return arrres
+    if step<0:
+        while i > end:
+            arr.append(i)
+            i += step
 
-		if len(args) == 2:
-			if callable(args[1]):
-				arrres = []
-				for item in arr:
-					if args[1](item):
-						arrres.append(item)
-				return arrres
+    if len(args):
+        if callable(args[0]):
+            arrres = []
+            for item in arr:
+                if args[0](item):
+                    arrres.append(item)
+            return arrres
 
-		return arr
+    if len(args) == 2:
+        if callable(args[1]):
+            arrres = []
+            for item in arr:
+                if args[1](item):
+                    arrres.append(item)
+            return arrres
+
+    return arr
 
 
 """
@@ -69,15 +80,19 @@ def filterOnlyHighFive(in_num):
 
 
 if __name__ == '__main__':
-	print(myRangeGenerator.range(1, 6))
-	print(myRangeGenerator.range(6, 1, -1))
-	print(myRangeGenerator.range(1, 100))
-	print(myRangeGenerator.range(1, 6, 5))
-	print(myRangeGenerator.range(1, 2, 5))
-	print(myRangeGenerator.range(1, 10, 5, filterEvenNum))
-	print(myRangeGenerator.range(1, 10, filterEvenNum))
-	print(myRangeGenerator.range(1, 10, 5, filterOddNum))
-	print(myRangeGenerator.range(1, 10, filterOnlyHighFive))
-	print(myRangeGenerator.range(1, 100, filterOnlyHighFive))
-	print(myRange(1, 6))
-	
+    print(range(1, 6))
+    print(range(6, 1, -1))
+    print(range(1, 100))
+    print(range(1, 6, 5))
+    print(range(1, 2, 5))
+    print(range(1, 10, 5, filterEvenNum))
+    print(range(1, 10, filterEvenNum))
+    print(range(1, 10, 5, filterOddNum))
+    print(range(1, 10, filterOnlyHighFive))
+    print(range(1, 100, filterOnlyHighFive))
+    print(list(myRange(1, 6)))
+    print(list(myRange(6, 1, -1)))
+    R = myRange(1, 10, 5, filterEvenNum)
+    print(6 in R)
+    print(7 in R)
+    
